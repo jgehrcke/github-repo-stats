2f7a68f76a04a818fa2bdc6883754afb5c2dbed9
Dr. Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2020-12-27 17:59:40 +0100

    analyze.py: wip for pandoc-based md report
    Goal: generate HTML document with
    pandoc, using a pandoc template.
    Fill in content using markdown.
    Embed Vega charts straight via raw
    HTML used within markdown doc.
    Use GitHub CSS for overall appearance.
    
    Re-using concepts from
    https://github.com/jgehrcke/dcos-dev-prod-analysis

--- analyze.py
+++ analyze.py
@@ -16,9 +16,13 @@
 import argparse
 import logging
 import os
+import textwrap
 import json
+import subprocess
+import shutil
 import sys
 from datetime import datetime
+from io import StringIO
 
 import pandas as pd
 from github import Github
@@ -45,8 +49,13 @@ logging.basicConfig(
     datefmt="%y%m%d-%H:%M:%S",
 )
 
+NOW = datetime.utcnow()
+TODAY = NOW.strftime("%Y-%m-%d")
+OUTDIR = None
+
 
 def main():
+
     args = parse_args()
     log.info("read views/clones time series fragments (CSV docs)")
     log.info("number of csv files provided: %s", len(args.csvpath))
@@ -133,98 +142,45 @@ def main():
     # print(df_agg)
 
     ## .mark_area(color="lightblue", interpolate="step-after", line=True)
-    ##.mark_line(point=True)
+    # pip carbonplan[styles]
+
+    ##
+    PANEL_WIDTH = 350
     chart_clones_unique = (
         alt.Chart(df_agg_clones)
-        .mark_area(
-            line={"color": "darkgreen"},
-            point=True,
-            color=alt.Gradient(
-                gradient="linear",
-                stops=[
-                    alt.GradientStop(color="white", offset=0),
-                    alt.GradientStop(color="darkgreen", offset=1),
-                ],
-                x1=1,
-                x2=1,
-                y1=1,
-                y2=0,
-            ),
-        )
+        .mark_line(point=True)
         .encode(
             alt.X("time", type="temporal"),
             alt.Y("clones_unique", type="quantitative", title="unique clones per day"),
         )
-    ).properties(height=200)
+    ).properties(height=200, width=PANEL_WIDTH)
 
     chart_clones_total = (
         alt.Chart(df_agg_clones)
-        .mark_area(
-            line={"color": "darkgreen"},
-            point=True,
-            color=alt.Gradient(
-                gradient="linear",
-                stops=[
-                    alt.GradientStop(color="white", offset=0),
-                    alt.GradientStop(color="darkgreen", offset=1),
-                ],
-                x1=1,
-                x2=1,
-                y1=1,
-                y2=0,
-            ),
-        )
+        .mark_line(point=True)
         .encode(
             alt.X("time", type="temporal"),
             alt.Y("clones_total", type="quantitative", title="total clones per day"),
         )
-    ).properties(height=200)
+    ).properties(height=200, width=PANEL_WIDTH)
 
     chart_views_unique = (
         alt.Chart(df_agg_views)
-        .mark_area(
-            line={"color": "darkgreen"},
-            point=True,
-            color=alt.Gradient(
-                gradient="linear",
-                stops=[
-                    alt.GradientStop(color="white", offset=0),
-                    alt.GradientStop(color="darkgreen", offset=1),
-                ],
-                x1=1,
-                x2=1,
-                y1=1,
-                y2=0,
-            ),
-        )
+        .mark_line(point=True)
         .encode(
             alt.X("time", type="temporal"),
             alt.Y("views_unique", type="quantitative", title="unique views per day"),
         )
-    ).properties(height=200)
+    ).properties(height=200, width=PANEL_WIDTH)
 
     chart_views_total = (
         alt.Chart(df_agg_views)
-        .mark_area(
-            line={"color": "darkgreen"},
-            point=True,
-            color=alt.Gradient(
-                gradient="linear",
-                stops=[
-                    alt.GradientStop(color="white", offset=0),
-                    alt.GradientStop(color="darkgreen", offset=1),
-                ],
-                x1=1,
-                x2=1,
-                y1=1,
-                y2=0,
-            ),
-        )
+        .mark_line(point=True)
         .encode(
             alt.X("time", type="temporal"),
             alt.Y("views_total", type="quantitative", title="total views per day"),
         )
-    ).properties(height=200)
+    ).properties(height=200, width=PANEL_WIDTH)
 
     # alt.vconcat(
     #     alt.hconcat(chart_clones_unique, chart_clones_total),
@@ -238,17 +194,93 @@ def main():
         alt.vconcat(chart_views_unique, chart_views_total)
         .resolve_scale(x="shared")
         .properties(title="Views"),
-    ).save("chart.html")
+    ).save("chart.html", embed_options={"renderer": "svg"})
 
     # https://github.com/altair-viz/altair/issues/1422#issuecomment-525866028
     # chart.show()
     # chart_clones_total.save("chart.html")
 
+    now_text = NOW.strftime("%Y-%m-%d %H:%M UTC")
+    markdownreport = StringIO()
+    markdownreport.write(
+        textwrap.dedent(
+            f"""
+    % Repository statistics
+    %
+    % {now_text}
+    The report is generated based on GitHub traffic data for the foo/bar repository.
+    The code for generating this report lives in
+    [`github-repo-stats`](https://github.com/jgehrcke/github-repo-stats).
+    """
+        ).strip()
+    )
+
+    markdownreport.write(textwrap.dedent(
+    """
+
+
+    # Views
+
+
+    """))
+
+
+        spec1=chart1.to_json(indent=None),
+        spec2=chart2.to_json(indent=None),
+
+    report_md_text = markdownreport.getvalue()
+    md_report_filepath = os.path.join(OUTDIR, TODAY + "_report.md")
+    log.info("Write generated Markdown report to: %s", md_report_filepath)
+    with open(md_report_filepath, "wb") as f:
+        f.write(report_md_text.encode("utf-8"))
+
+    log.info("Copy resources directory into output directory")
+    shutil.copytree(args.resources_directory, os.path.join(OUTDIR, "resources"))
+
+    html_report_filepath = os.path.splitext(md_report_filepath)[0] + ".html"
+    log.info("Trying to run Pandoc for generating HTML document")
+    pandoc_cmd = [
+        args.pandoc_command,
+        "--toc",
+        "--standalone",
+        "--template=resources/template.html",
+        md_report_filepath,
+        "-o",
+        html_report_filepath,
+    ]
+    log.info("Running command: %s", " ".join(pandoc_cmd))
+    p = subprocess.run(pandoc_cmd)
+    if p.returncode == 0:
+        log.info("Pandoc terminated indicating success")
+    else:
+        log.info("Pandoc terminated indicating error")
+
 
 def parse_args():
+    global OUTDIR
     parser = argparse.ArgumentParser(description="")
     parser.add_argument("csvpath", nargs="+")
+    parser.add_argument("--pandoc-command", default="pandoc")
+    parser.add_argument("--resources-directory", default="resources")
+    parser.add_argument("--output-directory", default=TODAY + "_report")
     args = parser.parse_args()
+
+    if os.path.exists(args.output_directory):
+        if not os.path.isdir(args.output_directory):
+            log.error(
+                "The specified output directory path does not point to a directory: %s",
+                args.output_directory,
+            )
+            sys.exit(1)
+
+        log.info("Remove output directory: %s", args.output_directory)
+        shutil.rmtree(args.output_directory)
+
+    log.info("Create output directory: %s", args.output_directory)
+    os.makedirs(args.output_directory)
+
+    OUTDIR = args.output_directory
+
     return args
 
 
